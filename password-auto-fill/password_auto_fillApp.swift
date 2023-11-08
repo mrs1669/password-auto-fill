76c471fc6e4edbd824d5e3c8b6a39e95a7bb867b
Takumi Muraishi <mrs1669.develop@gmail.com>
2023-11-09 03:26:45 +0900

    [feat] create project

---
+++ password-auto-fill/password_auto_fillApp.swift
@@ -0,0 +1,17 @@
+//
+//  password_auto_fillApp.swift
+//  password-auto-fill
+//
+//  Created by 村石 拓海 on 2023/11/09.
+//
+
+import SwiftUI
+
+@main
+struct password_auto_fillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
